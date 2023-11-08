b8e3c604f26fc0e85b78aff24e4b2be016986b93
omarnero <omarnerou@gmail.com>
2023-11-08 09:41:41 +0200

    create Button

--- src/Pages/HomePage/Home.jsx
+++ src/Pages/HomePage/Home.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import './Home.css'
+import React from 'react';
+import './Home.css';
+import MainBtn from '../../Components/MainBtn/MainBtn';
 
 const Home = () => {
   return (
+    <main>
+      <MainBtn shadow={false}> Login </MainBtn>
+    </main>
+  );
+};
 
-    <main></main>
-  )
-}
-
-export default Home
+export default Home;
