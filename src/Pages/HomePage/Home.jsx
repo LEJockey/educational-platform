873eee25eefdb66b3b3b0579c8a9ce788f5c4618
omarnero <omarnerou@gmail.com>
2023-11-08 11:14:11 +0200

    intail footer

--- src/Pages/HomePage/Home.jsx
+++ src/Pages/HomePage/Home.jsx
@@ -5,12 +5,14 @@ import MainBtn from '../../Components/MainBtn/MainBtn';
 import './Home.css';
 import BgImg from './../../Components/BgImg/BgImg';
 import logo from '../../assests/Logo.png';
+import FooterLinks from '../../Components/FooterLinks/FooterLinks';
 
 const Home = () => {
   return (
     <main>
       <MainBtn shadow={false}> Login </MainBtn>
-      <BgImg src={logo} />
+      {/* <BgImg src={logo} /> */}
+      <FooterLinks />
     </main>
   );
 };
